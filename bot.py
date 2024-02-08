0b9b34fdf4368d52bef1bf30a0be96cc59796e48
Soebb <87156166+Soebb@users.noreply.github.com>
2024-02-08 14:46:28 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,10 +58,10 @@ async def tag(bot, m):
         image_data = None
 
     await mes.delete()
-    fname = await bot.ask(m.chat.id,'`Send the Filename`', filters=filters.text, parse_mode='Markdown')
-    title = await bot.ask(m.chat.id,'`Send the Title name`', filters=filters.text, parse_mode='Markdown')
-    artist = await bot.ask(m.chat.id,'`Send the Artist(s) name`', filters=filters.text, parse_mode='Markdown')
-    answer = await bot.ask(m.chat.id,'`Send the Artwork or` /skip', filters=filters.photo | filters.text, parse_mode='Markdown')
+    fname = await bot.ask(m.chat.id,'`Send the Filename`', filters=filters.text)
+    title = await bot.ask(m.chat.id,'`Send the Title name`', filters=filters.text)
+    artist = await bot.ask(m.chat.id,'`Send the Artist(s) name`', filters=filters.text)
+    answer = await bot.ask(m.chat.id,'`Send the Artwork or` /skip', filters=filters.photo | filters.text)
     music.remove_tag('artist')
     music.remove_tag('title')
     music['artist'] = artist.text
